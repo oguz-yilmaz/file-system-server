4855bae193521a81d73c517eb68400f51d23870e
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2024-06-17 16:44:09 +0300

    Updates errors

--- pkg/fsmod/errors.go
+++ pkg/fsmod/errors.go
@@ -1,5 +1,18 @@
 package fsmod
 
+const (
+	GenericErrorCode         = 100
+	StatusNotFoundCode       = 404
+	StatusInvalidRequestCode = 400
+)
+
+var errorMessages = map[int]string{
+	GenericErrorCode:         "something went wrong",
+	StatusNotFoundCode:       "file not found",
+	StatusInvalidRequestCode: "invalid parameters",
+}
+
+// FileNotFoundError is an error type for file not found
 type FileNotFoundError struct {
 	FileName   string
 	StatusCode int
@@ -13,7 +26,41 @@ func (e *FileNotFoundError) Error() string {
 func NewFileNotFoundError(fileName string) *FileNotFoundError {
 	return &FileNotFoundError{
 		FileName:   fileName,
-		StatusCode: 404, // todo: use a constant
-		Message:    "File not found",
+		StatusCode: StatusNotFoundCode,
+		Message:    errorMessages[StatusNotFoundCode],
+	}
+}
+
+// InvalidParamsError is an error type for invalid parameters
+type InvalidParamsError struct {
+	StatusCode int
+	Message    string
+}
+
+func (e *InvalidParamsError) Error() string {
+	return e.Message
+}
+
+func NewInvalidParamsError() *InvalidParamsError {
+	return &InvalidParamsError{
+		StatusCode: StatusInvalidRequestCode,
+		Message:    errorMessages[StatusInvalidRequestCode],
+	}
+}
+
+// GenericError is an error type for generic errors
+type GenericError struct {
+	StatusCode int
+	Message    string
+}
+
+func (e *GenericError) Error() string {
+	return e.Message
+}
+
+func NewGenericError() *GenericError {
+	return &GenericError{
+		StatusCode: GenericErrorCode,
+		Message:    errorMessages[GenericErrorCode],
 	}
 }
